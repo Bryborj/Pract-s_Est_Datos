e192a8b550f64ce336bf30f0694421496dd8d32c
Bryan Borges <bryanborjes@outlook.com>
2026-02-25 05:49:04 -0600

    Primera carga de proyectos

--- T5_Recursividad/laberinto/main.cpp
+++ T5_Recursividad/laberinto/main.cpp
@@ -1,9 +1,11 @@
 //backtracking
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
-// mapa
-char laberinto [5][5] = {
+#define COLS 5
+#define ROWS 5
+char laberinto [ROWS][COLS] = {
     {'I', ' ', '#', ' ', ' '},
     {'#', ' ', '#', ' ', '#'},
     {' ', ' ', ' ', ' ', ' '},
@@ -17,27 +19,45 @@ char laberinto [5][5] = {
 // ' ' = camino
 
 bool buscarSalida(int x, int y) {
-    if (laberinto[x][y] == NULL)
+    if (x < 0 || x >= ROWS || y< 0 || y >= COLS)
         return false; // fuera de los límites del laberinto
     if (laberinto[x][y] == '#' || laberinto[x][y] == '*')
         return false; // pared o ya visitado
     if (laberinto[x][y] == 'S') 
         return true; // salida encontrada
-    if (laberinto[x][y] == ' ')
+    if (laberinto[x][y] != 'I')
         laberinto[x][y] = '*'; // marcar como visitado
-    return buscarSalida(x - 1, y) || buscarSalida(x + 1, y) || buscarSalida(x, y - 1) || buscarSalida(x, y + 1);
+    if (buscarSalida(x - 1, y) || 
+        buscarSalida(x + 1, y) ||
+        buscarSalida(x, y - 1) ||
+        buscarSalida(x, y + 1))
+        return true;
+
+    return false;
 }
 
 int main(int argc, char const *argv[])
 {
-    cout << "Ejecutando";
-    buscarSalida(0,0);
-    printf("Laberinto:\n");
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            printf("%c ", laberinto[i][j]);
+    try
+    {
+        cout << "Ejecutando\n" << endl;
+        if (buscarSalida(0,0))
+            cout << "Salida encontrada!" << endl;
+        else
+            cout << "No hay salida." << endl;
+
+        printf("Mapa final:\n");
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                printf("%c ", laberinto[i][j]);
+            }
+            printf("\n");
         }
-        printf("\n");
+        return 0;
     }
-    return 0;
+    catch(const std::exception& e)
+    {
+        std::cerr << "ha ocurrido un error inesperado: {e.what()}" << '\n';
+    }
+    
 }
