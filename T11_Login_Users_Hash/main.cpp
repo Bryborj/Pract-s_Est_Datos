f5c45e292503d0741c88f088f366246a665cb5aa
Bryan Borges <bryanborjes@outlook.com>
2026-05-03 02:08:08 -0600

    feat: implement basic user structure and password hashing logic in main.cpp

---
+++ T11_Login_Users_Hash/main.cpp
@@ -0,0 +1,41 @@
+/*
+ * Archivo: main.cpp
+ * Autor: Bryan Albino Borges
+ * Fecha: 02/05/2026
+ * Descripcion: Implementacion de un sistema de usuarios con hashing
+*/
+
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+struct User {
+    string username;
+    long passwordHash;
+};
+
+string hashUsername(string user){
+    int hash = 0;
+    int p = 31;
+    int m = 0;
+    for (char c : user){
+        hash += c * pow(p, c);
+    }
+    return hash % m;
+}
+
+long long hashPassword(string pass){
+    long long hash = 0;
+    int p = 31;
+    int m = 0;
+    for (char c : pass){
+        hash += c * pow(p, c);
+    }
+    return hash % m;
+}
+
+void postUser(){
+
+}
