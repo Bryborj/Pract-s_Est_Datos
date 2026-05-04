88f3324d50bd349a44b265e3e03458c3f6849cdd
Bryan Borges <bryanborjes@outlook.com>
2026-05-03 22:12:07 -0600

    Fixed function hashUsernane for Hashing Table m, and eval (hash = (hash * p + c) % m)

--- T11_Login_Users_Hash/main.cpp
+++ T11_Login_Users_Hash/main.cpp
@@ -8,34 +8,47 @@
 #include <iostream>
 #include <string>
 #include <math.h>
+#include <vector>
 
 using namespace std;
 
+
+const int m = 10;
 struct User {
     string username;
-    long passwordHash;
+    long long passwordHash;
+    
+    User() {
+        username = "":
+        passwordHash = 0;
+    }
 };
+
+vector<User> table (m);
 
 int hashUsername(string user){
     int hash = 0;
     int p = 31;
-    int m = 0;
     for (char c : user){
-        hash += c * pow(p, c);
+        hash = (hash * p + c) % m;
     }
-    return hash % m;
+    return hash;
 }
 
 long long hashPassword(string pass){
     long long hash = 0;
     int p = 31;
-    int m = 0;
     for (char c : pass){
         hash += c * pow(p, c);
     }
-    return hash % m;
+    return hash;
 }
 
-void postUser(){
+void postUser(string user, string pass){
+    int index = hashUsername(user);
 
+}
+
+int main(){
+    cout << "Sistema de Autenticación Segura" << endl;
 }
