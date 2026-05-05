886c49224be160a2a7de1dc93c0fb797d06596a8
Bryan Borges <bryanborjes@outlook.com>
2026-05-05 02:48:25 -0600

    Refactoring structure for working v1 proyect

---
+++ Proyecto_Final/v1/main.cpp
@@ -0,0 +1,25 @@
+/*
+Bryan Albino Borges
+Estructuras de Datos
+Problema: Generador y Resolutor Visual de Laberintos CLI
+version: 1.0.0
+Starging proyect: 2026-05-05
+End proyect: ----
+*/
+
+#include <iostream>
+#include <vector>
+#include <SFML/Graphics.hpp>
+#include <queue>
+
+using namespace std;
+
+
+
+// Backtracking Recursivo
+
+
+int main() {
+    cout << "Generador y Resolutor Visual de Laberintos CLI" << endl;
+    return 0;
+}
